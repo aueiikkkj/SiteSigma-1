e5d5c50976cf0e9e3c4b8739fa5b0833f3d25d2c
Sr-Pao <kalilelias3@gmail.com>
2022-08-18 12:24:34 -0300

    atualização18/08

--- admin/categoria-salvar.php
+++ admin/categoria-salvar.php
@@ -14,8 +14,31 @@ if( isset($_GET['id']) || !empty($_GET['id'])){
 }
 include_once '_menu.php';
 ?>
-    <main>
-        <h2>Administração das Categorias</h2>
-        <a href="categoria-lista.php">Listagem</a>
+<main>
+    <h2>Administração das Categorias</h2>
+    <a href="categoria-lista.php">Listagem</a>
+    <hr>
+    <form action="categoria-processa.php" method="post" enctype="multipart/form-data">
+        <input type= "hiden" value="salvar" name= "acao">
+        <input type= "text" value="<?php echo $dados['Imagem'];?>" name="imagem">
+        <input type= "text" name="id" value="<?php echo $id;?>"><br>
+        <label for="nome">Nome:</label><br>
+        <input type="text" id="nome" name="nome" value="<?php echo $dados['Nome'];?>"><br>
+        <label for="descricao">Descrição:</label><br>
+        <textarea id="descricao" name="descricao"><?php echo $dados['Descricao'];?></textarea><br>
+        <label for="imagem"> Imagem:</label><br>
+        <?php
+        if( !empty($dados['Imagem']) ){
+        ?>
+            <img scr="../imagens/categorias/<?php echo $dados['Imagem'];?>" width="150" /><br>
+        <?php
+        }
+        ?>
+        <input type="file" name="foto">
         <hr>
-        <form action="categoria-processa.php" method="post" enctype="multipart/form-data">
+        <input type="submit" value="Enviar">
+    </form>
+</main>
+<?php
+include_once '_footer.php';
+?>
--- admin/categorias-processa.php
+++ admin/categorias-processa.php
@@ -4,3 +4,23 @@ include_once '../includes/_dados.php';
 $acao = $_REQUEST['acao'];
 $id = $_REQUEST['ID'];
 
+switch ($acao) {
+
+    case 'excluir':
+        $sql = "DELETE FROM categorias WHERE categoriaID = ".$sid;
+        mysqli_query($conexao, $sql);
+        header('location: ./categoria-lista.php');
+
+        break
+
+    case 'salvar':
+        $nome = $_POST['nome'];
+        $descricao = $POST['descricao'];
+
+        if($_FILES['foto']['size'] > 0){}
+            $uploaddir = '../imagens/categorias/';
+            $extensao = pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+            $nomearquivo = 'categoria-'.$id.'-'.rand().'.'.$extensao;
+            $uploadfile = $uploaddir . $nomearquivo;
+            move_uploaded_file($_FILES['foto']['tmp_name'], $uploadfile);
+}
